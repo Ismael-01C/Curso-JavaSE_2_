4cb49305a547c05b5cf216b3145de8d1cc1de7a7
Pasante6 desa <p6@rgtdom.local>
2022-08-17 15:00:44 -0500

    tipos de datos

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,5 +1,18 @@
 public class DataTypes {
     public static void main(String[] args) {
+
+        //Tipo de dato entero
         int n = 1234567890;
+        //Tipo de dato long
+        long nL = 123456789012L;
+        //para diferenciar el tipo de dato int, se le agrega la L de long al final
+
+        //Tipo de dato Decimal
+        //calculos matematicos con mayor presicion
+        Double nD = 123.1412;
+        //
+        float nF = 123.1412F;
+        //para diferenciar el tipo de dato Double, se le agrega la F de float al final
+
     }
 }
